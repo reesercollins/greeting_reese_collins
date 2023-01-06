3894e35f65e3697cab7f5cd2b6211a952041c179
Reese <10563996+reesercollins@users.noreply.github.com>
2023-01-06 13:53:12 -0500

    BW2-670: Add JPA annotations and HSQLDB for data storage (#2)

--- src/main/java/com/example/GreetingApplication.java
+++ src/main/java/com/example/GreetingApplication.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.hsqldb.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GreetingApplication {
 
 	public static void main(String[] args) {
+		Server server = new Server();
+		server.setDatabaseName(0, "maindb");
+		server.setDatabasePath(0, "mem:maindb");
+		server.setPort(9001);
+		server.start();
 		SpringApplication.run(GreetingApplication.class, args);
 	}
 
