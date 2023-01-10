156e898e1ba27d9ea561fbbd22dd1a5c4a6b12cd
reesercollins <10563996+reesercollins@users.noreply.github.com>
2023-01-10 10:30:13 -0500

    BW2-673: Create new endpoint for viewing past greetings

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -1,9 +1,13 @@
 package com.example.restservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -11,9 +15,25 @@ public class GreetingController {
 
     private static final String template = "Hello, %s";
 
+    GreetingRepository greetingRepository;
+
+    @Autowired
+    public void setRepository(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
     @GetMapping("/greeting")
+    public List<Greeting> getGreetings() {
+        List<Greeting> result = new ArrayList<>();
+        greetingRepository.findAll().forEach(result::add);
+        return result;
+    }
+
+    @PostMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(String.format(template, name));
+        Greeting greeting = new Greeting(String.format(template, name));
+        greetingRepository.save(greeting);
+        return greeting;
     }
 
 }
