68e42fd0f3af9388ce199b95401aad30f9456081
Reese <10563996+reesercollins@users.noreply.github.com>
2023-01-17 11:27:22 -0500

    Add tests using in-memory database (#8)

--- src/test/java/com/example/GreetingRepositoryTests.java
+++ src/test/java/com/example/GreetingRepositoryTests.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @RunWith(SpringRunner.class)
@@ -20,15 +21,18 @@ public class GreetingRepositoryTests {
 
     @Test
     public void whenFindingCustomerById_thenCorrect() {
-        greetingRepository.save(new Greeting("John"));
-        assertTrue(greetingRepository.findById(1L).isPresent());
-        assertEquals(greetingRepository.findById(1L).get().getContent(), "John");
+        Greeting greeting = greetingRepository.save(new Greeting("John"));
+        assertTrue(greetingRepository.findById(greeting.getId()).isPresent());
+        assertEquals(greetingRepository.findById(greeting.getId()).get().getContent(), "John");
     }
 
     @Test
     public void whenFindingAllCustomers_thenCorrect() {
-        greetingRepository.save(new Greeting("John"));
-        greetingRepository.save(new Greeting("Julie"));
+        Greeting greeting1 = greetingRepository.save(new Greeting("John"));
+        Greeting greeting2 = greetingRepository.save(new Greeting("Julie"));
         assertTrue(greetingRepository.findAll().iterator().hasNext());
+        assertNotEquals(greeting1.getId(), 0);
+        assertNotEquals(greeting2.getId(), 0);
+        assertNotEquals(greeting1.getId(), greeting2.getId());
     }
 }
